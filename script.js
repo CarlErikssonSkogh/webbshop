85b8b7ff2dff5f91f3bbd7bded2ec73e2b88aa3f
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2023-02-08 15:55:44 +0100

    försökte fixa sidebar

---
+++ script.js
@@ -0,0 +1,4 @@
+const navBar = document.querySelector("nav"),
+menuBtns = document.querySelectorAll(".meny-icon"),
+overlay = document.querySelector(".meny-icon");
+console.log(navBar, menuBtns, overlay);
