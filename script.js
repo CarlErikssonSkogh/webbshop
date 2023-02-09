0923dc3d0bad5eabf9abf4f55e01128649570cff
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2023-02-09 16:01:23 +0100

    fixade en sidebar

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const navBar = document.querySelector("nav"),
-menuBtns = document.querySelectorAll(".meny-icon"),
-overlay = document.querySelector(".meny-icon");
-console.log(navBar, menuBtns, overlay);
+const openSidebar = () => {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+};
+
+const closeSidebar = () => {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+};
