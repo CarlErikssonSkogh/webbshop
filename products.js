f13b49db08364e62ea30272b2b0a74f22d191bdb
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2023-02-24 18:43:02 +0100

    Gjorde klart hemsidan
    Webbshoppen är nu färdig

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "videogame 1",
+    price: 59.99,
+    instock: 7,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/videgameImage.png",
+  },
+  {
+    id: 1,
+    name: "videogame 2",
+    price: 24.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/videgameImage.png",
+  },
+  {
+    id: 2,
+    name: "videogame 3",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/videgameImage.png",
+  },
+  {
+    id: 3,
+    name: "videogamet 4",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/videgameImage.png",
+  },
+  {
+    id: 4,
+    name: "videogame 5",
+    price: 29.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/videgameImage.png",
+  },
+  {
+    id: 5,
+    name: "videogame 6",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/videgameImage.png",
+  },
+];
